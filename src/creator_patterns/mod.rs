6235d290d475670bb9efc815b9d942ae2d343706
James Carson <jms.crsn@gmail.com>
2016-02-04 23:56:02 -0500

    some stuff i was messing around with at the airport

--- src/creator_patterns/mod.rs
+++ src/creator_patterns/mod.rs
@@ -1,3 +1,46 @@
+enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
+trait MazeObject {
+    fn enter();
+}
+
+struct Room {
+    room_number: u8,
+    sides:       [MazeObject; 4],
+}
+
+impl Room {
+    fn get_side(&self, direction: Direction) {
+
+    }
+    fn set_side(&self, direction: Direction, maze_object: &Direction) {
+    }
+}
+
+struct Wall;
+
+struct Door {
+    open: bool,
+    room1: Room,
+    room2: Room,
+}
+
+struct Maze {
+    rooms: Vec<Room>,
+}
+
+impl Maze {
+    fn get_room(n: u8) {
+    }
+}
+
+struct MazeGame;
+
 #[test]
 fn foo() {
 }
